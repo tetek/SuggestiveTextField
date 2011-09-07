0ebf3f0effe3ffbc6e0ae1ae6812b26dfb524b97
Wojciech Mandrysz <tetek1@gmail.com>
2011-09-08 01:34:00 +0200

    Initial Commit

---
+++ AutoComplete/AutoCompleteAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AutoCompleteAppDelegate.h
+//  AutoComplete
+//
+//  Created by Wojciech Mandrysz on 08/09/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AutoCompleteAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ AutoComplete/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AutoComplete
+//
+//  Created by Wojciech Mandrysz on 08/09/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
