a17063da0a42a17fbd83954caf21b8e4902b72ea
Wojciech Mandrysz <tetek@ttk-pro.local>
2014-06-12 16:35:01 +0200

    fixed freeze issue. Changed delegation to new object.

--- AutoComplete/SuggestiveTextField.h
+++ AutoComplete/SuggestiveTextField.h
@@ -1,20 +1,27 @@
 //
-//  SuggestionMenu.h
-//  AutoComplete
+//  SuggestiveTextField.h
+//  SuggestiveTextField
 //
 //  Created by Wojciech Mandrysz on 19/09/2011.
-//  Copyright 2011 http://tetek.me . All rights reserved.
+//  Copyright 2011 http://tetek.wordpress.com . All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
 
-@interface SuggestiveTextField : UITextField <UITextFieldDelegate, UITableViewDataSource, UITableViewDelegate>
+@interface SuggestiveTextField
+    : UITextField <UITextFieldDelegate, UITableViewDataSource,
+                   UITableViewDelegate>
 
 // Set suggestions list of NSString's.
-- (void)setSuggestions:(NSArray*)suggestionStrings;
+- (void)setSuggestions:(NSArray *)suggestionStrings;
 
 // Set Custom popover size.
 - (void)setPopoverSize:(CGSize)size;
+
+// Filter array and reload TableView
+- (void)matchStrings:(NSString *)letters;
+
+// Present PopOver
+- (void)showPopOverList;
 
 // Define if popover should hide after user selects a suggestion.
 @property BOOL shouldHideOnSelection;
--- AutoComplete/SuggestiveTextField.m
+++ AutoComplete/SuggestiveTextField.m
@@ -1,22 +1,48 @@
 //
-//  SuggestionMenu.m
-//  AutoComplete
+//  SuggestiveTextField.m
+//  SuggestiveTextField
 //
 //  Created by Wojciech Mandrysz on 19/09/2011.
-//  Copyright 2011 http://tetek.me . All rights reserved.
+//  Copyright 2011 http://tetek.wordpress.com . All rights reserved.
 //
 
 #import "SuggestiveTextField.h"
 
 #define DEFAULT_POPOVER_SIZE CGSizeMake(300, 300)
 
+@interface SuggestiveDelegate : NSObject <UITextFieldDelegate>
+@property(weak) SuggestiveTextField *textField;
+@end
+
+@implementation SuggestiveDelegate
+
+- (BOOL)shouldChangeTextInRange:(UITextRange *)range
+                replacementText:(NSString *)text {
+  return YES;
+}
+- (BOOL)textField:(UITextField *)textField
+    shouldChangeCharactersInRange:(NSRange)range
+                replacementString:(NSString *)string {
+
+  NSMutableString *text = [NSMutableString stringWithString:textField.text];
+  [text replaceCharactersInRange:range withString:string];
+
+  [self.textField matchStrings:text];
+  [self.textField showPopOverList];
+
+  return YES;
+}
+
+@end
+
 @interface SuggestiveTextField ()
 
+@property(strong) NSArray *stringsArray;
+@property(strong) NSArray *matchedStrings;
+@property(strong) UIPopoverController *popOver;
+@property(strong) UITableViewController *controller;
 
-@property (strong) NSArray *stringsArray;
-@property (strong) NSArray *matchedStrings;
-@property (strong) UIPopoverController *popOver;
-@property (strong) UITableViewController *controller;
+@property SuggestiveDelegate *midDelegate;
 
 @end
 
@@ -24,111 +50,110 @@
 
 #pragma mark - Setup
 
-- (id)initWithCoder:(NSCoder *)aDecoder{
-    
-    if ((self = [super initWithCoder:aDecoder])) {
-        [self setup];
-    }
-    return self;
+- (id)initWithCoder:(NSCoder *)aDecoder {
+
+  if ((self = [super initWithCoder:aDecoder])) {
+    [self setup];
+  }
+  return self;
 }
-- (id)initWithFrame:(CGRect)frame{
-    
-    if ((self = [super initWithFrame:frame])) {
-        [self setup];
-    }
-    return self;
+- (id)initWithFrame:(CGRect)frame {
+
+  if ((self = [super initWithFrame:frame])) {
+    [self setup];
+  }
+  return self;
 }
-- (id)init{
-    
-    if ((self = [super init])) {
-        [self setup];
-    }
-    return self;
+- (id)init {
+
+  if ((self = [super init])) {
+    [self setup];
+  }
+  return self;
 }
 
-- (void)setup{
+- (void)setup {
 
-    self.delegate = self;
-    
-    self.matchedStrings = [NSArray array];
-    self.controller = [[UITableViewController alloc] initWithStyle:UITableViewStylePlain];
-    _controller.tableView.delegate = self;
-    _controller.tableView.dataSource = self;
+  self.midDelegate = [SuggestiveDelegate new];
+  self.midDelegate.textField = self;
+  self.delegate = self.midDelegate;
 
-    self.popOver = [[UIPopoverController alloc] initWithContentViewController:_controller];
-    
-    // Default values
-    _popOver.popoverContentSize = DEFAULT_POPOVER_SIZE;
-    self.shouldHideOnSelection = NO;
+  self.matchedStrings = [NSArray array];
+  self.controller =
+      [[UITableViewController alloc] initWithStyle:UITableViewStylePlain];
+  _controller.tableView.delegate = self;
+  _controller.tableView.dataSource = self;
+  self.autocorrectionType = UITextAutocorrectionTypeNo;
+  self.popOver =
+      [[UIPopoverController alloc] initWithContentViewController:_controller];
+
+  // Default values
+  _popOver.popoverContentSize = DEFAULT_POPOVER_SIZE;
+  self.shouldHideOnSelection = YES;
 }
 
 #pragma mark - Modifiers
 
-- (void)setSuggestions:(NSArray *)suggestionStrings{
-    self.stringsArray = suggestionStrings;
+- (void)setSuggestions:(NSArray *)suggestionStrings {
+  self.stringsArray = suggestionStrings;
 }
-- (void)setPopoverSize:(CGSize)size{
-    self.popOver.popoverContentSize = size;
+- (void)setPopoverSize:(CGSize)size {
+  self.popOver.popoverContentSize = size;
 }
 
 #pragma mark - Matching strings and Popover
 
 - (void)matchStrings:(NSString *)letters {
-    if (_stringsArray.count > 0) {
+  if (_stringsArray.count > 0) {
 
-        self.matchedStrings = [_stringsArray filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"self contains[cd] %@",letters]];
-        [_controller.tableView reloadData];
-    }
+    self.matchedStrings = [_stringsArray
+        filteredArrayUsingPredicate:
+            [NSPredicate predicateWithFormat:@"self contains[cd] %@", letters]];
+    [_controller.tableView reloadData];
+  }
 }
 
-- (void)showPopOverList{
+- (void)showPopOverList {
 
-    if (_matchedStrings.count == 0) {
-        [_popOver dismissPopoverAnimated:YES];
-    }
-    else if (!_popOver.isPopoverVisible){
-        [_popOver presentPopoverFromRect:self.frame inView:self.superview permittedArrowDirections:UIPopoverArrowDirectionUp animated:YES];
-    }
+  if (_matchedStrings.count == 0) {
+    [_popOver dismissPopoverAnimated:YES];
+  } else if (!_popOver.isPopoverVisible) {
+    [_popOver presentPopoverFromRect:self.frame
+                              inView:self.superview
+            permittedArrowDirections:UIPopoverArrowDirectionUp
+                            animated:YES];
+  }
 }
-
-
-#pragma mark - TextField Delegate
-
-- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
-    
-    NSMutableString *text = [NSMutableString stringWithString:textField.text];
-    [text replaceCharactersInRange:range withString:string];
-    
-    [self matchStrings:text];
-    [self showPopOverList];
-    
-    return YES;
-}
-
 
 #pragma mark - TableView Delegate & DataSource
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return _matchedStrings.count;
+- (NSInteger)tableView:(UITableView *)tableView
+    numberOfRowsInSection:(NSInteger)section {
+  return _matchedStrings.count;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    static NSString *CellIdentifier = @"Cell";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-    }
-    
-    cell.textLabel.text = [_matchedStrings objectAtIndex:indexPath.row];
-    
-    return cell;
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  static NSString *CellIdentifier = @"Cell";
+
+  UITableViewCell *cell =
+      [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+  if (cell == nil) {
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                  reuseIdentifier:CellIdentifier];
+  }
+
+  cell.textLabel.text = [_matchedStrings objectAtIndex:indexPath.row];
+
+  return cell;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    [self setText:[_matchedStrings objectAtIndex:indexPath.row]];
-    [_popOver dismissPopoverAnimated:_shouldHideOnSelection];
+- (void)tableView:(UITableView *)tableView
+    didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+  [self setText:[_matchedStrings objectAtIndex:indexPath.row]];
+  if (_shouldHideOnSelection) {
+    [_popOver dismissPopoverAnimated:YES];
+  }
 }
-
 
 @end
